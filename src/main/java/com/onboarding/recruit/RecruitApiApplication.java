615dba22e248ceb23f29dd170621e83fef88ccad
sh <madst0614@gmail.com>
2023-10-02 05:49:07 +0900

    init

---
+++ src/main/java/com/onboarding/recruit/RecruitApiApplication.java
@@ -0,0 +1,21 @@
+package com.onboarding.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RecruitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitApiApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+
+}
