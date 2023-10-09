684d8cbf42fcd8bfd277a755ff3cd68a26f277a2
sh <madst0614@gmail.com>
2023-10-09 17:02:08 +0900

    Test : CompanyMapper
    임의 Company 추가 Mapper 테스트

---
+++ src/test/java/com/onboarding/recruit/mapper/CompanyMapperTests.java
@@ -0,0 +1,32 @@
+package com.onboarding.recruit.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.onboarding.recruit.domain.CompanyVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Log4j2
+public class CompanyMapperTests {
+
+	@Setter(onMethod_ = { @Autowired })
+	private CompanyMapper mapper;
+
+	@Test
+	public void testInsert() {
+		for (int i = 0; i < 10; i++) {
+			CompanyVO company = new CompanyVO();
+			company.setCname(i + "");
+			log.info(company);
+			mapper.insert(company);
+		}
+
+	}
+}
