49735fc91960ad71f48f75b03fc7029316ee14d1
sh <madst0614@gmail.com>
2023-10-11 03:32:44 +0900

    feat BoardService
    요구사항 1,2,3,4,5 추가
    
    Test 필요

---
+++ src/main/java/com/onboarding/recruit/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.onboarding.recruit.service;
+
+import java.util.List;
+
+import com.onboarding.recruit.dto.BoardDTO;
+
+public interface BoardService {
+	
+	public int register(BoardDTO boardDTO);
+	
+	public int modify(BoardDTO boardDTO);
+	
+	public int remove(BoardDTO boardDTO);
+	
+	public List<BoardDTO> getAllBoardList(String keyword);
+	
+	public BoardDTO getBoardDetail(BoardDTO boardDTO);
+}
---
+++ src/main/java/com/onboarding/recruit/service/BoardServiceImpl.java
@@ -0,0 +1,107 @@
+package com.onboarding.recruit.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.onboarding.recruit.domain.BoardVO;
+import com.onboarding.recruit.dto.BoardDTO;
+import com.onboarding.recruit.exception.CommonErrorCode;
+import com.onboarding.recruit.exception.CustomException;
+import com.onboarding.recruit.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@AllArgsConstructor
+@Log4j2
+public class BoardServiceImpl implements BoardService {
+
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+
+	@Transactional
+	@Override
+	public int register(BoardDTO boardDTO) {
+		log.info("<Board Service> register " + boardDTO);
+		BoardVO boardVO = BoardVO.builder().cid(boardDTO.getCid()).cName(boardDTO.getCName())
+				.content(boardDTO.getContent()).position(boardDTO.getPosition()).build();
+		log.info("DTO TO VO" + boardVO);
+
+		return mapper.insert(boardVO);
+	}
+
+	@Transactional
+	@Override
+	public int modify(BoardDTO boardDTO) {
+		log.info("<Board Service> modify " + boardDTO);
+		BoardVO board = mapper.getBoardByBno(boardDTO.getBno());
+
+		if (board == null)
+			throw new CustomException(CommonErrorCode.BOARD_NOT_FOUND);
+
+		BoardVO boardVO = BoardVO.builder().cid(boardDTO.getCid()).cName(boardDTO.getCName())
+				.content(boardDTO.getContent()).position(boardDTO.getPosition()).build();
+		log.info("DTO TO VO" + boardVO);
+
+		return mapper.update(boardVO);
+	}
+
+	@Transactional
+	@Override
+	public int remove(BoardDTO boardDTO) {
+		log.info("<Board Service> remove " + boardDTO);
+
+		BoardVO board = mapper.getBoardByBno(boardDTO.getBno());
+
+		if (board == null)
+			throw new CustomException(CommonErrorCode.BOARD_NOT_FOUND);
+
+		return mapper.delete(boardDTO.getBno());
+	}
+
+	@Transactional
+	@Override
+	public List<BoardDTO> getAllBoardList(String keyword) {
+		log.info("<Board Service> getAllBoardList with " + keyword);
+
+		List<BoardDTO> allBoardList = new ArrayList<BoardDTO>();
+
+		List<BoardVO> voList = mapper.getAllBoardList(keyword);
+
+		voList.forEach(vo -> allBoardList.add(BoardDTO.builder().bno(vo.getBno()).cName(vo.getCName())
+				.position(vo.getPosition()).regDate(vo.getRegDate()).build()));
+
+		return allBoardList;
+	}
+
+	@Transactional
+	@Override
+	public BoardDTO getBoardDetail(BoardDTO boardDTO) {
+		log.info("<Board Service> getBoardDetail which bno is " + boardDTO.getBno());
+		List<BoardVO> boardList = mapper.getBoardDetail(boardDTO.getCid());
+
+		List<Integer> companyBoardList = new ArrayList<Integer>();
+		BoardVO board = null;
+
+		for (BoardVO vo : boardList) {
+			if (vo.getBno() != boardDTO.getBno()) {
+				companyBoardList.add(vo.getBno());
+			} else {
+				board = vo;
+			}
+		}
+		boardDTO.setCompanyBoardList(companyBoardList);
+		
+		if (board == null)
+			throw new CustomException(CommonErrorCode.BOARD_NOT_FOUND);
+		
+		boardDTO.setContent(board.getContent());
+		return boardDTO;
+	}
+}
