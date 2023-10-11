c46ec3d9c9867eab0cff4e3b467c3f9474eb59e5
sh <madst0614@gmail.com>
2023-10-12 04:44:02 +0900

    Refactor BoardDTO
    toVO 추가

--- src/main/java/com/onboarding/recruit/dto/BoardDTO.java
+++ src/main/java/com/onboarding/recruit/dto/BoardDTO.java
@@ -3,12 +3,16 @@ package com.onboarding.recruit.dto;
 import java.util.Date;
 import java.util.List;
 
+import com.onboarding.recruit.domain.BoardVO;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -19,7 +23,22 @@ public class BoardDTO {
 	private String position;
 	private String content;
 	private Date regDate;
-	
+
 	// 같은 회사 다른 공고
 	private List<Integer> companyBoardList;
+
+	public BoardDTO(BoardVO vo, List<Integer> companyBoardList) {
+		this.bno = vo.getBno();
+		this.cid = vo.getCid();
+		this.cName = vo.getCName();
+		this.position = vo.getPosition();
+		this.content = vo.getContent();
+		this.regDate = vo.getRegDate();
+		this.companyBoardList = companyBoardList;
+	}
+
+	public BoardVO toVO() {
+		return BoardVO.builder().bno(this.bno).cid(this.cid).cName(this.cName).position(this.position)
+				.content(this.content).regDate(this.regDate).build();
+	}
 }
