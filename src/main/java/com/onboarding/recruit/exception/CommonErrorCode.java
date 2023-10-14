c19c6b136754c3f6eac05ce4ac5db4944f9e808f
sh <madst0614@gmail.com>
2023-10-14 18:10:44 +0900

    add apply error code

--- src/main/java/com/onboarding/recruit/exception/CommonErrorCode.java
+++ src/main/java/com/onboarding/recruit/exception/CommonErrorCode.java
@@ -8,9 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
-	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
-	BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "Board not exists"),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid Parameter Included"),
+	ALREADY_APPLIED(HttpStatus.CONFLICT, "Already Applied"),
+	BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "Board Not Exists"),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
 
 	private final HttpStatus httpStatus;
 	private final String message;
