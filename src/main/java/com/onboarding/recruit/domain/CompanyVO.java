2d034393d11a5fb34f4cee968b80ff156cc834d7
sh <madst0614@gmail.com>
2023-10-14 18:12:26 +0900

    Refactor add builder, delete setter

--- src/main/java/com/onboarding/recruit/domain/CompanyVO.java
+++ src/main/java/com/onboarding/recruit/domain/CompanyVO.java
@@ -1,8 +1,12 @@
 package com.onboarding.recruit.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class CompanyVO {
 	private int cid;
 	private String cname;
